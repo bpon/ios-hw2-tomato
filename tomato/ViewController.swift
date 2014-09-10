6b05672b84b765b334fac45170d82a5335fb077d
Bryan Pon <bpon@yahoo-inc.com>
2014-09-09 18:45:32 -0700

    Initial Commit

---
+++ tomato/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tomato
+//
+//  Created by Bryan Pon on 9/9/14.
+//  Copyright (c) 2014 bpon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
